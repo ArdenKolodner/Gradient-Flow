207f5721d373ec9f037bf1710f8acb4bac0cf792
Arden Kolodner <akolodner25@amherst.edu>
2023-06-18 14:58:07 -0400

    Initial Commit

---
+++ Gradient Flow/Gradient_FlowView.h
@@ -0,0 +1,12 @@
+//
+//  Gradient_FlowView.h
+//  Gradient Flow
+//
+//  Created by Arden Kolodner on 6/18/23.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Gradient_FlowView : ScreenSaverView
+
+@end
