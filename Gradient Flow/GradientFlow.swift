9fd5d571598042ad4d5a65b5da740d3822bbae0e
Arden Kolodner <akolodner25@amherst.edu>
2023-06-19 00:04:27 -0400

    Initial commit

--- Gradient Flow/GradientFlow.swift
+++ Gradient Flow/GradientFlow.swift
@@ -8,29 +8,63 @@
 import ScreenSaver
 
 class GradientFlow: ScreenSaverView {
-    private var redPos: CGPoint = .zero
-    private var greenPos: CGPoint = .zero
-    private var bluePos: CGPoint = .zero
+    private var redPos: [CGPoint]
+    private var greenPos: [CGPoint]
+    private var bluePos: [CGPoint]
     
-    private var redVel: CGVector = .zero
-    private var greenVel: CGVector = .zero
-    private var blueVel: CGVector = .zero
+    private var redVel: [CGVector]
+    private var greenVel: [CGVector]
+    private var blueVel: [CGVector]
+    
+    //private var numProjsPerColor = 2
+    private var numRed: Int
+    private var numGreen: Int
+    private var numBlue: Int
     
     private var squareSize = 50
     
     private var perturbEveryNFrames = 120
     private var frameCount = 0
-
+    
     // MARK: - Initialization
     override init?(frame: NSRect, isPreview: Bool) {
-        super.init(frame: frame, isPreview: isPreview)
-        redPos = CGPoint(x: frame.width / 2, y: frame.height / 2)
-        greenPos = CGPoint(x: frame.width / 2, y: frame.height / 2)
-        bluePos = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        redPos = []
+        greenPos = []
+        bluePos = []
+        redVel = []
+        greenVel = []
+        blueVel = []
         
-        redVel = initialVelocity()
-        greenVel = initialVelocity()
-        blueVel = initialVelocity()
+        numRed = Int.random(in: 0 ... 2)
+        numGreen = Int.random(in: 0 ... 2)
+        numBlue = Int.random(in: 0 ... 2)
+        
+        print("r: " + String(numRed))
+        print("g: " + String(numGreen))
+        print("b: " + String(numBlue))
+        
+        super.init(frame: frame, isPreview: isPreview)
+        
+        if numRed > 0 {
+            for _ in 1 ... numRed {
+                redPos.append(CGPoint(x: frame.width / 2, y: frame.height / 2))
+                redVel.append(initialVelocity())
+            }
+        }
+        
+        if numGreen > 0 {
+            for _ in 1 ... numGreen {
+                greenPos.append(CGPoint(x: frame.width / 2, y: frame.height / 2))
+                greenVel.append(initialVelocity())
+            }
+        }
+        
+        if numBlue > 0 {
+            for _ in 1 ... numBlue {
+                bluePos.append(CGPoint(x: frame.width / 2, y: frame.height / 2))
+                blueVel.append(initialVelocity())
+            }
+        }
     }
     
     private func initialVelocity() -> CGVector {
@@ -64,12 +98,24 @@ class GradientFlow: ScreenSaverView {
         if frameCount >= perturbEveryNFrames {
             frameCount = 0
             
-            redVel.dx += CGFloat.random(in: -1.0...1.0)
-            redVel.dy += CGFloat.random(in: -1.0...1.0)
-            greenVel.dx += CGFloat.random(in: -1.0...1.0)
-            greenVel.dy += CGFloat.random(in: -1.0...1.0)
-            blueVel.dx += CGFloat.random(in: -1.0...1.0)
-            blueVel.dy += CGFloat.random(in: -1.0...1.0)
+            if numRed > 0 {
+                for i in 0 ... numRed-1 {
+                    redVel[i].dx += CGFloat.random(in: -1.0...1.0)
+                    redVel[i].dy += CGFloat.random(in: -1.0...1.0)
+                }
+            }
+            if numGreen > 0 {
+                for i in 0 ... numGreen-1 {
+                    greenVel[i].dx += CGFloat.random(in: -1.0...1.0)
+                    greenVel[i].dy += CGFloat.random(in: -1.0...1.0)
+                }
+            }
+            if numBlue > 0 {
+                for i in 0 ... numBlue-1 {
+                    blueVel[i].dx += CGFloat.random(in: -1.0...1.0)
+                    blueVel[i].dy += CGFloat.random(in: -1.0...1.0)
+                }
+            }
         }
         
 //        let ballRect = NSRect(x: redPos.x,
@@ -84,9 +130,28 @@ class GradientFlow: ScreenSaverView {
     }
     
     func colorAt(p: CGPoint) -> NSColor {
-        let r = distancePct(d: distanceBetweenPoints(p1: p, p2: redPos))
-        let g = distancePct(d: distanceBetweenPoints(p1: p, p2: greenPos))
-        let b = distancePct(d: distanceBetweenPoints(p1: p, p2: bluePos))
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        
+        if numRed > 0 {
+            for i in 0 ... numRed-1 {
+                r += distancePct(d: distanceBetweenPoints(p1: p, p2: redPos[i]))
+            }
+            r /= CGFloat(numRed)
+        }
+        if numGreen > 0 {
+            for i in 0 ... numGreen-1 {
+                g += distancePct(d: distanceBetweenPoints(p1: p, p2: greenPos[i]))
+            }
+            g /= CGFloat(numGreen)
+        }
+        if numBlue > 0 {
+            for i in 0 ... numBlue-1 {
+                b += distancePct(d: distanceBetweenPoints(p1: p, p2: bluePos[i]))
+            }
+            b /= CGFloat(numBlue)
+        }
         
         return NSColor(red: r, green: g, blue: b, alpha: 1)
     }
@@ -104,33 +169,58 @@ class GradientFlow: ScreenSaverView {
         super.animateOneFrame()
 
         // Update the "state" of the screensaver in this function
-        if redPos.x + redVel.dx > frame.width || redPos.x + redVel.dx < 0 {
-            redVel.dx *= -1
-        }
-        if redPos.y + redVel.dy > frame.height || redPos.y + redVel.dy < 0 {
-            redVel.dy *= -1
+        
+        if numRed != 0 {
+            for i in 0 ... numRed-1 {
+                if redPos[i].x + redVel[i].dx > frame.width || redPos[i].x + redVel[i].dx < 0 {
+                    redVel[i].dx *= -1
+                }
+                if redPos[i].y + redVel[i].dy > frame.height || redPos[i].y + redVel[i].dy < 0 {
+                    redVel[i].dy *= -1
+                }
+            }
         }
         
-        if bluePos.x + blueVel.dx > frame.width || bluePos.x + blueVel.dx < 0 {
-            blueVel.dx *= -1
-        }
-        if bluePos.y + blueVel.dy > frame.height || bluePos.y + blueVel.dy < 0 {
-            blueVel.dy *= -1
-        }
-        
-        if greenPos.x + greenVel.dx > frame.width || greenPos.x + greenVel.dx < 0 {
-            greenVel.dx *= -1
-        }
-        if greenPos.y + greenVel.dy > frame.height || greenPos.y + greenVel.dy < 0 {
-            greenVel.dy *= -1
+        if numBlue != 0 {
+            for i in 0 ... numBlue-1 {
+                if bluePos[i].x + blueVel[i].dx > frame.width || bluePos[i].x + blueVel[i].dx < 0 {
+                    blueVel[i].dx *= -1
+                }
+                if bluePos[i].y + blueVel[i].dy > frame.height || bluePos[i].y + blueVel[i].dy < 0 {
+                    blueVel[i].dy *= -1
+                }
+            }
         }
         
-        redPos.x += redVel.dx
-        redPos.y += redVel.dy
-        bluePos.x += blueVel.dx
-        bluePos.y += blueVel.dy
-        greenPos.x += greenVel.dx
-        greenPos.y += greenVel.dy
+        if numGreen != 0 {
+            for i in 0 ... numGreen-1 {
+                if greenPos[i].x + greenVel[i].dx > frame.width || greenPos[i].x + greenVel[i].dx < 0 {
+                    greenVel[i].dx *= -1
+                }
+                if greenPos[i].y + greenVel[i].dy > frame.height || greenPos[i].y + greenVel[i].dy < 0 {
+                    greenVel[i].dy *= -1
+                }
+            }
+        }
+            
+        if numRed != 0 {
+            for i in 0 ... numRed-1 {
+                redPos[i].x += redVel[i].dx
+                redPos[i].y += redVel[i].dy
+            }
+        }
+        if numBlue != 0 {
+            for i in 0 ... numBlue-1 {
+                bluePos[i].x += blueVel[i].dx
+                bluePos[i].y += blueVel[i].dy
+            }
+        }
+        if numGreen != 0 {
+            for i in 0 ... numGreen-1 {
+                greenPos[i].x += greenVel[i].dx
+                greenPos[i].y += greenVel[i].dy
+            }
+        }
         
         setNeedsDisplay(bounds);
     }
